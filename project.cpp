9b6a92c618147722fc960ea74dac138216e4392b
Florin Petrean <florin.petrean98@gmail.com>
2021-05-12 17:51:06 +0300

    initial

---
+++ project.cpp
@@ -0,0 +1,10 @@
+
+#include "project.h"
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <curses.h>
+#include <vector>
---
+++ project.h
@@ -0,0 +1,10 @@
+//
+// Created by Florin on 12/05/2021.
+//
+
+#ifndef OPENCV_PROJECT_H
+#define OPENCV_PROJECT_H
+
+
+
+#endif //OPENCV_PROJECT_H
